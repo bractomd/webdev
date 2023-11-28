f4bae91f53eb2b7526a1ceb826d0ec0d7bee2d83
Scholar <scholar@paradigm.local>
2023-11-28 14:25:05 -0700

    money

--- budget/budget.js
+++ budget/budget.js
@@ -1,18 +1,29 @@
 function calculateBudget() {
-    income = document.getElementById("begin").value;
-    firstexpense = document.getElementById("money1").value;
-    secondexpense = document.getElementById("money2").value;
-    thirdexpense = document.getElementById("money3").value;
-    forthexpense = document.getElementById("money4").value;
-    fithexpense = document.getElementById("money5").value;
-    sixthexpense = document.getElementById("money6").value;
+    let income = document.getElementById("begin").value;
+    let firstexpense = document.getElementById("money1").value;
+    let secondexpense = document.getElementById("money2").value;
+    let thirdexpense = document.getElementById("money3").value;
+    let forthexpense = document.getElementById("money4").value;
+    let fithexpense = document.getElementById("money5").value;
+    let sixthexpense = document.getElementById("money6").value;
    
 
 
-    Muns = income - firstexpense - secondexpense - thirdexpense - forthexpense - fithexpense - sixthexpense;
+    let Muns = income - firstexpense - secondexpense - thirdexpense - forthexpense - fithexpense - sixthexpense;
     document.getElementById(
         "left-over"
     ).innerHTML = `you have ${Muns} moneys lefty`;
+
+    if (Muns >= 0)
+        document.getElementById("advice1").innerHTML = `john is impressed with your money managing skills`
+    else if (Muns < 0)
+        document.getElementById("advice2").innerHTML = `john says you should get a job`
+
+
+    
+
+
+
 
     
 };
